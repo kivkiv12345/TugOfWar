0d660903edbffca32589c745b6d1018ddf985fbe
kivkiv12345 <kevincarlsen@live.dk>
2021-09-01 12:18:50 +0200

    Added score and process names and stuff.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from time import sleep
-from subprocess import call
+#from subprocess import call
 from multiprocessing.managers import ValueProxy
 from multiprocessing.synchronize import Event as EventHint
 from multiprocessing import Process, Event, Manager
 
-WIN_NUMBER = 100
+try:  # Change process names to something informational when setproctitle is installed.
+    from setproctitle import setproctitle
+except ImportError:  # setproctitle isn't installed, processes will use their default names.
+    setproctitle = None
+
+WIN_NUMBER = 200
 SLEEPTIME = 0.05
+
+score = [0, 0]
 
 
 class bcolors:
@@ -20,10 +27,16 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def tug(tug_progress: ValueProxy, change: int, done_flag: EventHint = False):
-    while not done_flag.is_set():
-        tug_progress.set(tug_progress.get() + change)
-        sleep(SLEEPTIME)
+def tug(tug_progress: ValueProxy, change: int, done_flag: EventHint = False, name: str = None):
+    try:
+        if setproctitle is not None and name:
+            setproctitle(name)
+
+        while not done_flag.is_set():
+            tug_progress.set(tug_progress.get() + change)
+            sleep(SLEEPTIME)
+    except KeyboardInterrupt:
+        print(f"Stopped: '{name}'")  # Suppress KeyboardInterrupt traceback.
 
 
 def main():
@@ -31,17 +44,21 @@ def main():
     progress = Manager().Value('i', WIN_NUMBER // 2)
     done_flag = Event()
 
-    tug_right = Process(target=tug, args=(progress, 1, done_flag))
-    tug_left = Process(target=tug, args=(progress, -1, done_flag))
+    tug_right = Process(target=tug, args=(progress, 1, done_flag, "Python | tug right"))
+    tug_left = Process(target=tug, args=(progress, -1, done_flag, "Python | tug left"))
 
     tug_right.start()
     tug_left.start()
 
-    while (current_progress := progress.get()) in range(WIN_NUMBER):
-        tug_string = bcolors.FAIL + '=' * current_progress
-        tug_string += bcolors.OKBLUE + '0'
-        tug_string += bcolors.OKGREEN + '=' * (WIN_NUMBER - current_progress)
-        print(tug_string)
+    if setproctitle is not None:
+        setproctitle("python | tug print")
+
+    while (current_progress := progress.get()) in range(WIN_NUMBER + 1):
+        print(f"{bcolors.FAIL}({score[0]})\t"
+              f"{'=' * current_progress}"
+              f"{bcolors.OKBLUE}{max(score) - min(score)}"  # 0
+              f"{bcolors.OKGREEN}{'=' * (WIN_NUMBER - current_progress)}"
+              f"\t({score[1]})")
         sleep(SLEEPTIME)
         #call(['clear'])  # Clears the console on UNIX systems; duh!
 
@@ -49,14 +66,26 @@ def main():
 
     if current_progress >= WIN_NUMBER:
         print(bcolors.OKGREEN + "Right side won!")
+        score[1] += 1
     else:
         print(bcolors.FAIL + "Left side won!")
+        score[0] += 1
 
     done_flag.set()
 
-    tug_right.join()
-    tug_left.join()
+    tug_right.join(timeout=SLEEPTIME * 10)
+    tug_left.join(timeout=SLEEPTIME * 10)
+
+    tug_right.terminate()
+    tug_left.terminate()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    # raise SystemExit()
+    try:
+        while True:
+            main()
+            sleep(3)
+    except KeyboardInterrupt:
+        pass  # Suppress the traceback.
